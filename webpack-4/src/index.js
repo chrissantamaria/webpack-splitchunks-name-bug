90b69a553247fa35063d4e9b530b698e879c306e
Chris Santamaria <chris@santamaria.me>
2021-05-17 14:20:53 -0400

    Add non-named three async chunk

--- webpack-4/src/index.js
+++ webpack-4/src/index.js
@@ -1,10 +1,13 @@
 Promise.all([
+  // Explicitly named with webpackChunkName (should not be named by lib chunk group)
   import(
     '@formatjs/intl-datetimeformat/polyfill' /* webpackChunkName: "polyfill-intl-datetimeformat" */
   ),
   import(
     '@formatjs/intl-datetimeformat/add-all-tz' /* webpackChunkName: "polyfill-intl-datetimeformat-add-all-tz" */
   ),
+  // No custom name (should be named by lib chunk group)
+  import('three'),
 ]).then(() => {
   console.log('Polyfills loaded');
 });
